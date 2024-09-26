4bd14a042db87fe35b9cfd2faabb423ddbf14c46
Jimin <jak2899@naver.com>
2024-09-26 15:59:14 +0900

    코딩애플 리액트 강의부분

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Hello from './Hello';
 
 function App() {
+
+  let post = "강남 우동 맛집";
+  let [글제목, changeName] = useState(['여자 코트 추천', '남자 코트 추천', '쇼핑몰 추천']);
+  let [좋아요, plusNumber] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <h4>블로그임</h4>
+      </div>
+      <h4 style={{background: "yellow", color: "red"}}>{post}</h4>
+
+      
+
+      <button onClick={()=>{
+        let copy = [...글제목];
+        copy[0] = '올해의 예상 트렌드';
+        changeName(copy);
+        }}>🔃</button>
+
+      <button onClick={()=>{
+        let copy = [...글제목];
+        copy.sort();
+        changeName(copy);
+      }}>정렬</button>
+
+      <div className='list'>
+      <h4>{글제목[0]}   <span onClick={() => {plusNumber(좋아요 + 1)}}>👍</span>   {좋아요}   </h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{글제목[1]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{글제목[2]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+
+      <Hello></Hello>
+      <Modal></Modal>
+
+    </div>
+  );
+}
+
+let Modal = () => {
+  return(
+    <div className='modal'>
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
     </div>
   );
 }
